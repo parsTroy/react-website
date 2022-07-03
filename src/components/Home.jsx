5275488cd323da46b60630c281b8d4114bd1f24a
parsTroy <troyparsons@hotmail.com>
2022-07-02 22:14:32 -0400

    Fix View Projects Button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -15,7 +16,9 @@ const Home = () => {
             </p>
             <div>
                 <button className='text-[#caccc1] hover:text-[#1b3338] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#bf874c] hover:border-[#bf874c]'>
-                    View Projects 
+                    <Link to="projects" smooth={true} duration={500} className='hover:text-[#bf874c]'>
+                        View Projects
+                    </Link>
                 <span className='group-hover:rotate-90 duration-300'>
                     <HiArrowNarrowRight className='ml-3' />
                 </span>
