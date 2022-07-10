c0fc12a8c392d643e788872afefbbd4d5935855c
parsTroy <troyparsons@hotmail.com>
2022-07-10 17:10:03 -0400

    click logo for home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,9 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#151a1d] text-[#caccc1]'>
         <div>
-            <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
+            <a href='./'>
+              <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
+            </a>
         </div>
 
         {/* menu */}
