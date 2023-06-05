eb8111a5922826f5b51e3bc089b6a0903474a045
parsTroy <troyparsons@hotmail.com>
2023-06-04 21:37:37 -0400

    Update contact conditions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,8 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#bf874c] text-[#caccc1]'>Contact</p>
                 <p className='text-[#caccc1] py-4'>// Submit the form below or send me an email - troyparsonsdev@proton.me</p>
             </div>
-            <input className='p-2 bg-[#669297] text-[#1b3338] placeholder-[#caccc1]' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#669297] text-[#1b3338] placeholder-[#caccc1]' type="email" placeholder='Email' name='email' />
+            <input className='p-2 bg-[#669297] text-[#1b3338] placeholder-[#caccc1]' type="text" placeholder='Name' name='name' required/>
+            <input className='my-4 p-2 bg-[#669297] text-[#1b3338] placeholder-[#caccc1]' type="email" placeholder='Email' name='email' required/>
             <textarea className='p-2 bg-[#669297] text-[#1b3338] placeholder-[#caccc1]' name='message' rows='10' placeholder='Message'></textarea>
             <button className='text-[#caccc1] border-[#caccc1] border-2 hover:bg-[#bf874c] hover:border-[#bf874c] px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
