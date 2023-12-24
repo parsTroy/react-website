bab27c34efffd8f1511323f9de499cbf05b0f93a
parsTroy <troyparsons@hotmail.com>
2023-12-24 11:37:59 -0500

    Udate assets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
 
 const Home = () => {
-  return (
-    <div name='home' className='w-full h-screen bg-[#151a1d]'>
-        
-        {/* Containor */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-[#bf874c]'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#FEF9F3]'>Troy Parsons</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#E8D9CA]'>I'm a Front End Developer.</h2>
-            <p className='text-[#E8D9CA] py-4 max-w-[700px]'>
-                Focused on growing my portfolio through working on meaningful projects, with purpose.
-            </p>
-            <div>
+    return (
+        <div name='home' className='w-full h-screen bg-[#151a1d]'>
+
+            {/* Containor */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className='text-[#bf874c]'>Hi, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#FEF9F3]'>Troy Parsons</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#E8D9CA]'>I'm a Full-Stack Developer.</h2>
+                <p className='text-[#E8D9CA] py-4 max-w-[700px]'>
+                    Focused on growing my portfolio through working on meaningful projects, with purpose.
+                </p>
                 <Link to="projects" smooth={true} duration={500} className='hover:text-[#E8D9CA]'>
-                    <button className='text-[#caccc1] hover:text-[#1b3338] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#bf874c] hover:border-[#bf874c]'>
+                    <div>
+                        <button className='text-[#caccc1] hover:text-[#1b3338] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#bf874c] hover:border-[#bf874c]'>
                             View Projects
-                    <span className='group-hover:rotate-90 duration-300'>
-                        <HiArrowNarrowRight className='ml-3' />
-                    </span>
-                    </button>
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <HiArrowNarrowRight className='ml-3' />
+                            </span>
+                        </button>
+                    </div>
                 </Link>
             </div>
-        </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Home
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DividendEg from '../assets/dividendPortfolioTracker'
+import DividendEg from '../assets/network.png'
 import CryptoEg from '../assets/crypto.png'
 import JSGameEg from '../assets/typingGame.png'
 import stockAppEg from '../assets/stock-app.png'
@@ -7,148 +7,148 @@ import golfTrackerApp from '../assets/golf-tracker-app.png'
 import ToDoEg from '../assets/todo.png'
 
 const Projects = () => {
-  return (
-    <div name='projects' className='w-full md:h-screen text-[#caccc1] bg-[#151a1d]'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-[#caccc1] border-[#bf874c]'>Projects</p>
-                <p className='py-6'>// Check out some of my recent projects</p>
-            </div>
+    return (
+        <div name='projects' className='w-full md:h-screen text-[#caccc1] bg-[#151a1d]'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 text-[#caccc1] border-[#bf874c]'>Projects</p>
+                    <p className='py-6'>// Check out some of my recent projects</p>
+                </div>
 
-        {/* Containor */}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-10'>
+                {/* Containor */}
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-10'>
 
-            {/* Grid Item */}    
-                <div 
-                style={{backgroundImage: `url(${DividendEg})`}}
-                className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
-                            Dividend Tracker
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://parstroy.github.io/react-stock-portfolio'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/parsTroy/react-stock-portfolio'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
-                            </a>
+                    {/* Grid Item */}
+                    <div
+                        style={{ backgroundImage: `url(${DividendEg})` }}
+                        className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
 
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
+                                Social Network
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://network-seven-gray.vercel.app/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/parsTroy/network'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
+                                </a>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div 
-                style={{backgroundImage: `url(${JSGameEg})`}}
-                className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
-                            Typing Game
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://parstroy.github.io/js-typer/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/parsTroy/js-typer'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
-                            </a>
+                    <div
+                        style={{ backgroundImage: `url(${JSGameEg})` }}
+                        className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
 
-                        </div>
-                    </div>   
-                </div>
-                           {/* Grid Item */}    
-                           <div 
-                style={{backgroundImage: `url(${CryptoEg})`}}
-                className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
-                            Crypto Tracker
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://react-cryptobase.web.app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/parsTroy/react-crypto-watchlist-app'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
-                            </a>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
+                                Typing Game
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://parstroy.github.io/js-typer/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/parsTroy/js-typer'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
+                                </a>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div 
-                style={{backgroundImage: `url(${golfTrackerApp})`}}
-                className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
-                            Golf Score Tracker
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://parstroy.github.io/responsive-golf-tracker/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/parsTroy/responsive-golf-tracker'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
-                            </a>
+                    {/* Grid Item */}
+                    <div
+                        style={{ backgroundImage: `url(${CryptoEg})` }}
+                        className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
 
-                        </div>
-                    </div>   
-                </div>
-                           {/* Grid Item */}    
-                           <div 
-                style={{backgroundImage: `url(${stockAppEg})`}}
-                className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
-                            Stock Tracker
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://parstroy.github.io/stock-app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/parsTroy/stock-app.git'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
-                            </a>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
+                                Crypto Tracker
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://react-cryptobase.web.app/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/parsTroy/react-crypto-watchlist-app'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
+                                </a>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div 
-                style={{backgroundImage: `url(${ToDoEg})`}}
-                className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
-                            React To-Do App
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://parstroy.github.io/firebase-react-todo-app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/parsTroy/firebase-react-todo-app'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
-                            </a>
+                    <div
+                        style={{ backgroundImage: `url(${golfTrackerApp})` }}
+                        className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
 
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
+                                Golf Score Tracker
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://parstroy.github.io/responsive-golf-tracker/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/parsTroy/responsive-golf-tracker'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
+                                </a>
+
+                            </div>
                         </div>
-                    </div>   
+                    </div>
+                    {/* Grid Item */}
+                    <div
+                        style={{ backgroundImage: `url(${stockAppEg})` }}
+                        className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
+                                Stock Tracker
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://parstroy.github.io/stock-app/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/parsTroy/stock-app.git'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{ backgroundImage: `url(${ToDoEg})` }}
+                        className='shadow-lg shadow-[#040c16] group containor rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-[#caccc1] tracking-wider'>
+                                React To-Do App
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://parstroy.github.io/firebase-react-todo-app/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/parsTroy/firebase-react-todo-app'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#caccc1] text-[#1b3338] font-bold text-lg'>Code</button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
-                
-
-
-
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Projects;
